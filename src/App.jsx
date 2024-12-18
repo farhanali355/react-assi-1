bda77a2ecf0b639f63936e572235d5519b32b86e
farhan <ff1917914@gmail.com>
2024-12-18 08:01:23 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () =>{
+  return(
+    <div>
+      <h1>hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolorum quaerat eius fugit!</p>
+      <h2>hello world</h2>
+      <p>Lorem, ipsum.</p>
+      <h3>hello world</h3>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  )
+}
+export default App;
